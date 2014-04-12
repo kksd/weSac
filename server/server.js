bd7bd97fc07042ee43a9c7a3e70932aff97cffd3
Xi Leon Chen <leon_chen@xyratex.com>
2014-04-12 11:11:03 -0400

    adding seperate module for calculation responder

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var routes = require('rapid-rest')();
+// This module is for calculation
+var Responder = require("./routes/responder.js");
+var responder = new Responder();
 
 var port = 4444;
 
 routes('/:id')
-	('get', function(req, res, params){
-		obj = {id: params.id};
-		res.writeHead(201, '', {'Content-Type': 'text/json'});
-		res.end(JSON.stringify(obj));
-	});
+    ('get', function(req, res, params){
+        obj = {id: params.id};
+        send(res, obj);
+    });
+
+function send(res, obj){
+    res.writeHead(201, '', {'Content-Type': 'text/json'});
+    res.end(JSON.stringify(obj));
+}
 
 routes.listen(port);
