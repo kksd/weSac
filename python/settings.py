902ba627de32919b0c71b160426156935e624f1a
Shengying Pan <i@thepans.me>
2014-04-13 02:28:29 -0400

    aa

--- python/settings.py
+++ python/settings.py
@@ -10,7 +10,7 @@ ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 #    SERVER_NAME = 'thepans.info'
 #else:
 #    SERVER_NAME = '10.172.248.91:8080'
-SERVER_NAME = ''
+SERVER_NAME = 'localhost:5000'
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_USERNAME = 'user'
@@ -22,4 +22,3 @@ DOMAIN = {
     'location' : location
 }
 
-print SERVER_NAME
