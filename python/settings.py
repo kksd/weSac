6d2d37463b4af01366f072a05630c3a1d8ca2dfe
Ubuntu <ubuntu@ip-172-31-0-70.us-west-2.compute.internal>
2014-04-13 06:20:32 +0000

    update

--- python/settings.py
+++ python/settings.py
@@ -5,19 +5,21 @@ from location import location
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
-print os.environ
 
-if os.environ.get('PORT'):
-    SERVER_NAME = 'www.thepans.info'
-else:
-    SERVER_NAME = '10.172.248.91:8080'
-    MONGO_HOST = 'localhost'
-    MONGO_PORT = 27017
-    MONGO_USERNAME = 'user'
-    MONGO_PASSWORD = 'user'
-    MONGO_DBNAME = 'eve'
+#if os.environ.get('USER') == 'ubuntu':
+#    SERVER_NAME = 'thepans.info'
+#else:
+#    SERVER_NAME = '10.172.248.91:8080'
+SERVER_NAME = ''
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'user'
+MONGO_PASSWORD = 'user'
+MONGO_DBNAME = 'eve'
 
 DOMAIN = {
     'asteroid' : asteroid,
     'location' : location
 }
+
+print SERVER_NAME
