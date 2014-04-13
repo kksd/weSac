a4282f3ec9d462999db93131c448e339e1af52af
Shengying Pan <i@thepans.me>
2014-04-13 05:40:06 -0400

    added a new service

--- python/hack.py
+++ python/hack.py
@@ -1,6 +1,11 @@
 from pymongo import MongoClient
 from datetime import datetime
+from collections import defaultdict
+from flask import Response
+from flask import request
 import flask
+import math
+import json
 
 client = MongoClient()
 db = client.eve
@@ -9,7 +14,27 @@ db = client.eve
 class Hacker(object):
 
 	def hack(self, app):
+		#This get current distance for all asteroids
 		@app.route('/current')
 		def index():
-			return 'Hello World'
+			result = []
+			#utc = datetime.utcnow()
+			#year = utc.year
+			#month = utc.month
+			#day = utc.day
+			#hour = utc.hour
+			#minute = utc.minute
+			#minute = int(math.floor(minute / 10.0) * 10.0)
+			#minute = 10 * math.floor(utc.minute / 10)
+			#now = datetime(year, month, day, hour, minute)
+			utc = request.args.get('utc', '')
+			utc = datetime.strptime(utc, '%Y-%b-%d %H:%M')
+			for item in db.location.find({"time" : utc}):
+				data = {
+					'aid' : item['aid'],
+					'lt' : item['lt']
+				}
+				result.append(data)
+			
+			return Response(json.dumps(result), mimetype='application/json')
 
